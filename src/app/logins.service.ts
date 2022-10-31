88d34368a0be5e9f468f33ce2be4987f59c74d25
Rich Eodice <rich@eodice.com>
2022-10-31 18:19:22 -0400

    Alpha 0.9 live

--- src/app/logins.service.ts
+++ src/app/logins.service.ts
@@ -5,29 +5,15 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class LoginsService {
-  private baseUrl = 'http://logins.api.dev.lsnj.org/';
+  private baseUrl = 'http://192.168.100.120:8000/';
 
   constructor(private httpClient: HttpClient) { }
-
-  getEvents(start: Date, end: Date,
-            machine: string, username: string,
-            startups: boolean, shutdowns: boolean, crashes: boolean, renames: boolean,
-            logons: boolean, logoffs: boolean, locks: boolean, unlocks: boolean,
-            take: number, skip: number) {
-    let endpoint = 'v1/events/';
-    endpoint = endpoint + '?machine=' + machine + '&username=' + username;
-    endpoint = endpoint + '&start=' + start.toISOString() + '&end=' + end.toISOString();
-    endpoint = endpoint + '&startups=' + startups + '&shutdowns=' + shutdowns + '&crashes=' + crashes + '&renames=' + renames;
-    endpoint = endpoint + '&logons=' + logons + '&logoffs=' + logoffs + '&locks=' + locks + '&unlocks=' + unlocks;
-    endpoint = endpoint + '&take=' + take + '&skip=' + skip + '&desc=true';
-    return this.httpClient.get(this.baseUrl + endpoint);
-  }
 
   getBasicEvents(start: Date, end: Date,
                  search: string,
                  logons: boolean, logoffs: boolean, locks: boolean, unlocks: boolean,
                  take: number, skip: number) {
-    let endpoint = 'v2/events/';
+    let endpoint = 'api/logins';
     endpoint = endpoint + '?search=' + search;
     endpoint = endpoint + '&start=' + start.toISOString() + '&end=' + end.toISOString();
     endpoint = endpoint + '&startups=' + 'true' + '&shutdowns=' + 'true' + '&crashes=' + 'true' + '&renames=' + 'true';
