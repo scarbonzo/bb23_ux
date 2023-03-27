78e860790737592a77aeefd71053d646133e6510
Rich Eodice <rich@eodice.com>
2023-03-27 09:41:22 -0400

    v1.1 done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BoardComponent } from './board/board.component';
 import { EditorComponent } from './editor/editor.component';
-import { IoboardComponent } from './ioboard/ioboard.component';
-import { IocreateComponent } from './iocreate/iocreate.component';
-import { IoeditorComponent } from './ioeditor/ioeditor.component';
 import { LoginsComponent } from './logins/logins.component';
 import { CreateComponent } from './create/create.component';
 import { LiveboardComponent } from './liveboard/liveboard.component';
@@ -12,20 +9,11 @@ import { LiveboardComponent } from './liveboard/liveboard.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'logins',
+    redirectTo: 'board',
     pathMatch: 'full'
   },{
     path: 'logins',
     component: LoginsComponent
-  },{
-    path: 'ioboard',
-    component: IoboardComponent
-  },{
-    path: 'iocreate',
-    component: IocreateComponent
-  },{
-    path: 'ioeditor/:username',
-    component: IoeditorComponent
   },{
     path: 'board',
     component: BoardComponent
